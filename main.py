d5874f71f685eab737dc0bd28b26b8f3c6cb3979
Jordan Hall <32490517+JordHall@users.noreply.github.com>
2020-08-24 22:12:55 +0100

    Added hashing and salting logic - in progress

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 import hashlib
+def isEven(num):
+    if ((num % 2) == 0):
+        return True
+    else:
+        return False
 prompt = input("Enter password: ")
-hash1 = hashlib.md5(prompt.encode())
-hash2 = hashlib.sha1(prompt.encode())
-hash3 = hashlib.sha256(prompt.encode())
-hashes = {hash1, hash2, hash3}
-for item in hashes:
-    print(item.hexdigest())
+hashedpw = hashlib.sha256(prompt.encode()).hexdigest()
+print(hashedpw)
+resultpw = ""
+index = 0
+for char in hashedpw:
+    if index == 3:
+        resultpw += "_"
+    elif index == 6: 
+        resultpw += "@"
+    elif index == 9:
+        resultpw += "/"
+    if isEven(index):
+        resultpw += char.upper()
+    else:
+        resultpw += char
+    index += 1
+print(resultpw)
