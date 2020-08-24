2116c3314a64f2fd00eab812db2a419407b2d017
Jordan Hall <32490517+JordHall@users.noreply.github.com>
2020-08-24 15:28:13 +0100

    reverted bcrypt, imported hashlib

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import bcrypt
-salt = bcrypt.gensalt()
-test = bcrypt.hashpw(b"test",salt)
-print(test)
+import hashlib
+prompt = input("Enter password: ")
+hash1 = hashlib.md5(prompt.encode())
+hash2 = hashlib.sha1(prompt.encode())
+hash3 = hashlib.sha256(prompt.encode())
+hashes = {hash1, hash2, hash3}
+for item in hashes:
+    print(item.hexdigest())
