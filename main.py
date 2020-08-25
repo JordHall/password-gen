9a1b57419515af6874725025cf824d7482396adf
Jordan Hall <32490517+JordHall@users.noreply.github.com>
2020-08-25 15:03:59 +0100

    Encapsulated functions, commenting and main loop

--- main.py
+++ main.py
@@ -1,24 +1,32 @@
 import hashlib
+
+## Returns true if number is even ##
 def isEven(num):
     if ((num % 2) == 0):
         return True
     else:
         return False
-prompt = input("Enter password: ")
-hashedpw = hashlib.sha256(prompt.encode()).hexdigest()
-print(hashedpw)
-resultpw = ""
-index = 0
-for char in hashedpw:
-    if index == 3:
-        resultpw += "_"
-    elif index == 6: 
-        resultpw += "@"
-    elif index == 9:
-        resultpw += "/"
-    if isEven(index):
-        resultpw += char.upper()
-    else:
-        resultpw += char
-    index += 1
-print(resultpw)
+
+## Hash input and splice in consistent modifications ##
+def hashPW(userInput):
+    hashedpw = hashlib.sha256(userInput.encode()).hexdigest()
+    resultpw = ""
+    index = 0
+    for char in hashedpw:
+        if index == 3:
+            resultpw += "_" # Add 3 symbols ##
+        elif index == 6: 
+            resultpw += "@"
+        elif index == 9:
+            resultpw += "/"
+        if isEven(index):
+            resultpw += char.upper() # Convert some to uppercase #
+        else:
+            resultpw += char
+        index += 1
+    print(resultpw)
+
+## Main loop ##
+while True:
+    prompt = input("Enter password: ")
+    hashPW(prompt)
